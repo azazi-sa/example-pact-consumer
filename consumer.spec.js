68d279a0aac0256ad951738102f595e220a063e9
bawaji94 <shabbirb@thoughtworks.com>
2019-04-16 15:45:29 +0530

    Shabbir | Added test for updating employees

--- consumer.spec.js
+++ consumer.spec.js
@@ -82,7 +82,7 @@ describe("Pact Tests for Employee Service", () => {
             });
         });
 
-        it("lists all the employees", done => {
+        it("fetch an employee", done => {
             const url = `${BASE_URL}/employees/1`;
             const method = "GET";
             const headers = {Accept: "application/json"};
@@ -117,7 +117,7 @@ describe("Pact Tests for Employee Service", () => {
             });
         });
 
-        it("lists all the employees", done => {
+        it("create an employee", done => {
             const url = `${BASE_URL}/employees`;
             const method = "PUT";
             const headers = {Accept: "application/json", "Content-Type": "application/json"};
@@ -127,6 +127,42 @@ describe("Pact Tests for Employee Service", () => {
                 .then(employee => {
                     expect(typeof employee).toEqual('object');
                     expect(employee.Id).toEqual("1");
+                })
+                .then(() => done())
+                .catch((err) => done(err));
+        });
+    });
+
+    describe('Update an employee', () => {
+        beforeEach(() => {
+            return provider.addInteraction({
+                state: "updating an employee",
+                uponReceiving: "a request for updating an employee",
+                withRequest: {
+                    method: "POST",
+                    path: "/employees/1",
+                    headers: {Accept: "application/json", "Content-Type": "application/json"},
+                    body: { Id: "1", Name: 'Ram Shinde' }
+                },
+                willRespondWith: {
+                    status: 200,
+                    headers: {"Content-Type": "application/json"},
+                    body: { Id: "1", Name: 'Ram Shinde' }
+                }
+            });
+        });
+
+        it("create an employee", done => {
+            const url = `${BASE_URL}/employees/1`;
+            const method = "POST";
+            const headers = {Accept: "application/json", "Content-Type": "application/json"};
+            const body = JSON.stringify({ Id: "1", Name: 'Ram Shinde' });
+            fetch(url, {method, headers, body})
+                .then(response => response.json())
+                .then(employee => {
+                    expect(typeof employee).toEqual('object');
+                    expect(employee.Id).toEqual("1");
+                    expect(employee.Name).toEqual('Ram Shinde');
                 })
                 .then(() => done())
                 .catch((err) => done(err));
